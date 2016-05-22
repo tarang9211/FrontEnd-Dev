22d8fa9f4213bea24e98c43d669cc1e839a3694f
Hirani, Tarang [USA] <taranghirani@C02R52JTG8WM.local>
2016-05-22 02:25:05 -0400

    icon animation for menu

--- dev/app.js
+++ dev/app.js
@@ -5,6 +5,10 @@ var app = express()
 app.use(express.static(__dirname + '/views'))
 app.use(express.static(__dirname + '/public'))
 
+app.get('/burger', function(req, res) {
+	res.sendFile(__dirname + '/views/burger.html')
+})
+
 //set up port
 app.listen(3000, function(){
 	console.log('Sample App running on port 3000')
--- dev/public/scripts/scripts.js
+++ dev/public/scripts/scripts.js
@@ -1,8 +1,10 @@
 (function(){
 	var burger = document.querySelector('.burger-container');
 	var headContainer = document.querySelector('.head-container');
-	
+
+
 	burger.onclick = function() {
 		headContainer.classList.toggle('menu-opened');
 	}
+
 }());
